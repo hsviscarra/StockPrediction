ca92a2a6d5a937d39980bfa40c6ba6835fd87805
Hernan Viscarra <hsviscarra@gmail.com>
2025-08-02 06:03:22 -0500

    Fix imports and PYTHONPATH for GitHub Actions and modular structure

--- tests/unit/test_api/test_predictions.py
+++ tests/unit/test_api/test_predictions.py
@@ -1,13 +1,16 @@
 from fastapi.testclient import TestClient
 from api.app import app
 
-
 client = TestClient(app)
 
 
-def test_predict_endpoint():
+def test_predict_status_code():
     response = client.get("/predict")
     assert response.status_code == 200
+
+
+def test_predict_endpoint_data():
+    response = client.get("/predict")
     data = response.json()
 
     assert "symbol" in data
